399e4a3cdb44813c57588d3c81b12be3ecee695a
Piotr Dąbrowski <piotrek816@wp.pl>
2018-02-15 12:37:27 +0100

    Add project files.

---
+++ Pakowanie LED/NewBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pakowanie_LED
+{
+    public partial class NewBox : Form
+    {
+        public NewBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
